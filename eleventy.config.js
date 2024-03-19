21544e804f7cebb808ffd390802b66c1d1751916
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2024-03-19 18:36:29 +0000

    Make the split output in a specific folder
    And configure 11ty to drop that extra folder when serving the files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,13 @@
 
-module.exports = function() {
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("content", {
+    filter: "**/*.{jpg,jpeg,png,svg}",
+    rename(filePath) {
+      return filePath.replace('doc-import/','');
+    }
+  });
+
+
   return {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
